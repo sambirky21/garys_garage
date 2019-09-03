5d8d80110e75c264f4261a8efd54a7ab855708eb
Samuel Birky <sbirky21@gmail.com>
2019-09-03 14:32:42 -0500

    it works

---
+++ vehicle.py
@@ -0,0 +1,6 @@
+class Vehicle():
+    def __init__(self, manufacturer, model, horsepower, wheel_count):
+        self.manufacturer = manufacturer
+        self.model = model
+        self.horsepower = horsepower
+        self.wheel_count = wheel_count
